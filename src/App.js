0142c3555ba11efe96c9a006e1c51a60decfe1e3
Justin Barfield <justin.kyle.barfield@gmail.com>
2020-10-12 14:50:33 -0400

    funcitons added to clear word, set word (api functionality pending api key), handle key down, and componentDidMount. clearWord and setWord are both within the comonentDidMount. handleKeyDown set to validate if key matches any of the letters in the word, validate if the letter is recurring, then change the state for attempts and repeat respectively.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Container, Row, Col} from 'react-bootstrap';
+import { Container, Row, Col, Alert } from 'react-bootstrap';
 import Title from './components/Title';
 import Attempts from './components/Attempts';
 import GamesWon from './components/GamesWon';
@@ -11,81 +11,154 @@ import Letters from './components/Letters';
 
 class App extends Component {
 
-state={
-    attempts: "",
-    wins: "",
-    losses: "",
-    picture: "",
-    word: ""
-    
-}
+    state={
+        attempts: [],
+        wins: "",
+        losses: "test",
+        picture: "",
+        word: [],
+        remainingAttempts: 6,
+        repeat: false,
+        
+    }
 
-// function for API word
-
-
-// function for updating states
-
-
-// function for breaking up word and breaking it out into the necessary components. May be easier to break word into array or obj then count how many letters have been selected correctly.
+    // function for API word
 
 
-// function to take key press, validate against word, and update state accordingly. If/else statement or condition will be needed.
+    // function for updating states
+
+    // Clear word state for new incoming word
+    clearWord = () => {
+        this.setState({ word: [] });
+        console.log(this.state.word);
+    };
+
+    // Set word state for new incoming word
+    setWord = () => {
+        const fullWord = "word";
+        const wordArray = fullWord.split("");
+        
+        console.log("wordArray inside setWord: ", wordArray);
+
+        this.setState({ word: wordArray });
+        
+    };
+
+    // Record attempts on key press
+    handleKeyDown = (event) => {
+        let match = false;
+        let repeat = false;
+        
+        // Validate if key pressed matches the word
+        this.state.word.map((value, index) => {
+            
+            if( this.state.word[index] === event.key ) {
+                return match = true;
+            }
+            
+        })
+
+        // Validate if the key pressed is recurring
+        this.state.attempts.map((value, index) => {
+
+            if( this.state.attempts[index] === event.key ) {
+                return repeat = true
+            }
+        })
 
 
-componentDidMount() {
-    
-}
+        // if repeat is false set attempts and repeat. else set repeat to true
+        if( !repeat ) {
 
-render(){
-    return (
+            this.setState({
+                attempts: this.state.attempts.concat(event.key),
+                repeat: false,
+            });
+        
+        } else {
 
-    <>
+            this.setState({
+                repeat: true,
+            })
 
-        <Container fluid={true}>
+        }
 
-            <Row>
 
-                <Title/>
-                
-            </Row>
+        console.log(this.state.attempts);
+    }
 
-            <Row>
 
-                <Col sm={2}>
 
-                    <Attempts/>
+    // function for breaking up word and breaking it out into the necessary components. May be easier to break word into array or obj then count how many letters have been selected correctly.
 
-                    <GamesWon/>
 
-                    <GamesLost/>
+    // function to take key press, validate against word, and update state accordingly. If/else statement or condition will be needed.
 
-                
-                </Col>
+    componentDidMount() {
+        this.clearWord();
+        this.setWord();
+        document.addEventListener("keydown", this.handleKeyDown)
+        
+    }
 
-                <Col sm={10}>
 
-                    <Row>
 
-                        <Hangman/>
+    render(){
+        return (
 
-                    </Row>
+        <>
 
-                    <Row>
+            <Container fluid={true}>
 
-                        <Letters/>
+                <Row>
 
-                    </Row>
+                    <Title/>
+                    
+                </Row>
 
-                </Col>
-                
-            </Row>
+                <Row>
 
-        </Container>
+                    <Col sm={2}>
 
-    </>
+                        <Attempts/>
 
-    )
-}
+                        <GamesWon/>
+
+                        <GamesLost/>
+
+                    
+                    </Col>
+
+                    <Col sm={10}>
+
+                        <Row>
+
+                            <Hangman/>
+
+                        </Row>
+
+                        <Row>
+
+                            {this.state.repeat ? (
+                                <Alert variant={"danger"}>
+                                    Repeat character used
+                                </Alert>
+                            ):(<></>)}
+
+                            <Letters propWord={this.state.word} />
+
+                        </Row>
+
+                    </Col>
+                    
+                </Row>
+
+            </Container>
+
+        </>
+
+        )
+    }
 
 }
 
