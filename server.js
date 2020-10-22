6c3d3ba0c6df302bd7b379dbc558b7e4090636e4
Justin Barfield <justin.kyle.barfield@gmail.com>
2020-10-22 12:27:27 -0400

    Heroku successfully recognizes the project but now encouters an error due to mime type in text/html. unused css imports removed from components. index.css contents have been deleted. app.test.js added back to project. In server, app.get for sending the index.html, the * has been removed.

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ app.get("/getWord", (req, res) => {
 
 
 //if no other routes are hit, send the react app
-app.get("*", (req, res) => {
+app.get( (req, res) => {
     res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 // router.use(function(req, res) {
