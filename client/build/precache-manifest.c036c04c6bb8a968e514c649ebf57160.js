8ab97dec798435fd83e4cd92e81e896bfc54706d
Justin Barfield <justin.kyle.barfield@gmail.com>
2020-10-22 12:40:07 -0400

    client build ran in effort to fix heroku error. Index.html rel changed to noose for the page icon. Server.js console logs removed.

---
+++ client/build/precache-manifest.c036c04c6bb8a968e514c649ebf57160.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bdbd08c00b996ecdeb867258baaa936",
+    "url": "/hangman/index.html"
+  },
+  {
+    "revision": "489835b2d3c2b904d31a",
+    "url": "/hangman/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "8396c177c6728cbe8362",
+    "url": "/hangman/static/css/main.84161af4.chunk.css"
+  },
+  {
+    "revision": "489835b2d3c2b904d31a",
+    "url": "/hangman/static/js/2.543b2bcf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hangman/static/js/2.543b2bcf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8396c177c6728cbe8362",
+    "url": "/hangman/static/js/main.8ea163a6.chunk.js"
+  },
+  {
+    "revision": "7ef95293683d0ed87720",
+    "url": "/hangman/static/js/runtime-main.6b115b9b.js"
+  }
+]);
